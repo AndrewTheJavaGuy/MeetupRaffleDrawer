962f3958498a862359cbd6bf7c208c952eae3f9a
AndrewTheJavaGuy <here_he_is@hotmail.com>
2021-06-03 06:54:43 +1000

    After talking with the Brainstrust on the code, got this so it would show a seed value used and you can get the same results by passing this the generated seed

--- src/main/java/au/com/meetup/drawer/RafflePicker.java
+++ src/main/java/au/com/meetup/drawer/RafflePicker.java
@@ -3,6 +3,7 @@ package au.com.meetup.drawer;
 
 import org.apache.commons.codec.digest.DigestUtils;
 import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.lang3.math.NumberUtils;
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.Row;
@@ -25,7 +26,9 @@ public class RafflePicker {
 
     private static final Row.MissingCellPolicy policy = Row.MissingCellPolicy.CREATE_NULL_AS_BLANK;
 
-    private static final SecureRandom random = new SecureRandom();
+    private static final String DIGEST_SEPERATOR = "------------------------";
+
+    private static final Random random = new Random();
 
     // For you non-programmers, everything starts at 0 instead of 1
     // https://blog.usejournal.com/arrays-start-at-zero-whats-up-with-that-f2d1054c9b77
@@ -45,15 +48,28 @@ public class RafflePicker {
             System.exit(-1);
         }
 
-        logOutput(String.format("Running with %s",args[0]));
+        logger.info(String.format("Running with %s",args[0]));
 
         File file = new File(args[0]);
 
         if (!file.canRead()) {
-            logOutput("File cannot be read");
+            logger.info("File cannot be read");
         }
 
         try {
+
+            // If you pass a seed as the 2nd argument, this should get the same results with the same data
+            Long seed;
+            if (args.length > 1 && args[1] != null && NumberUtils.isCreatable(args[1])) {
+                seed = Long.parseLong(args[1]);
+            } else {
+                seed = random.nextLong();
+            }
+
+            logger.info("Using the seed: "+ seed);
+            logger.info("Areas recorded in the digest will bet between the "+DIGEST_SEPERATOR);
+
+            random.setSeed(seed);
 
             Map<String,String> participants = new HashMap<>();
 
@@ -68,6 +84,9 @@ public class RafflePicker {
             Sheet sheet = workbook.getSheetAt(0);
 
             int totalRows = sheet.getLastRowNum();
+
+            logger.info("About to start the draw");
+            logOutput(DIGEST_SEPERATOR);
 
             // First row appears to be a header row
             for (int i=1;i<=totalRows;i++) {
@@ -127,6 +146,8 @@ public class RafflePicker {
             });
             logOutput("Thanks to everyone who entered");
 
+            logOutput(DIGEST_SEPERATOR);
+
             logger.info("Final digest of all output (excluding this line): " +
                     Base64.getEncoder().encodeToString(DigestUtils.sha512(finalOutput.toString())));
 
@@ -146,7 +167,7 @@ public class RafflePicker {
      * @param str
      */
     private static void logOutput(String str) {
-        logger.info(str+"\n");
-        finalOutput.append(str+"\n");
+        logger.info(str);
+        finalOutput.append(str);
     }
 }
